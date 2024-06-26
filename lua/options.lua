814ab9100c97795a3d03961891d79ca9637fc8ba
simon-lentz <slentz445@gmail.com>
2024-06-26 13:55:56 -0400

    Adds pane navigation keymaps to options.lua

--- lua/options.lua
+++ lua/options.lua
@@ -37,8 +37,18 @@ local options = function()
     vim.opt.timeoutlen = 300
 
     -- Configure how new splits should be opened.
+    -- :vsp
     vim.opt.splitright = true
+    -- :sp
     vim.opt.splitbelow = true
+
+    -- Keybinds to make split navigation easier.
+    --  Use CTRL+<hjkl> to switch between windows
+    --  See `:help wincmd` for a list of all window commands
+    vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+    vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+    vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+    vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
     -- Show which line your cursor is on.
     vim.opt.cursorline = true
