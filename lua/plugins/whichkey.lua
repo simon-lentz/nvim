1e359d276fad8b60ab7049bccd115a53a78d4329
simon-lentz <slentz445@gmail.com>
2024-06-26 14:27:14 -0400

    Adds alpha plugin in dashboard.lua, fixes WhichKey binding

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -5,5 +5,8 @@ return {
     config = function()
       -- Set up plugin.
       require('which-key').setup()
+      require('which-key').register({
+        ['wk'] = { '<cmd>WhichKey<cr>', 'Open keymaps menu.' },
+      })
     end,
 }
