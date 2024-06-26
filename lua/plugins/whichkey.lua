ae4b5582040e71c98c9e429c14303fb9fa8e0bf5
simon-lentz <slentz445@gmail.com>
2024-06-26 13:46:34 -0400

    Adds telescope plugin and associated keybindings, removes redundant code from whichkey.lua

--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -3,22 +3,7 @@ return {
     'folke/which-key.nvim',
     event = 'VimEnter',
     config = function()
+      -- Set up plugin.
       require('which-key').setup()
-
-      -- Keybindings used with stevearc/oil.nvim (oil.lua) plugin.
-      require('which-key').register({
-        ['<leader>otb'] = { '<cmd>Oil<cr>', 'Open directory tree as editable buffer.' },
-      })
-
-      -- Keybindings used with nvim-tree/nvim-tree.lua (filetree.lua) plugin.
-      require('which-key').register({
-        ['<leader>ot'] = { '<cmd>NvimTreeOpen<cr>', 'Open directory tree.' },
-      })
-
-      -- Keybindings used with voldikss/vim-floaterm (term.lua) plugin.
-      require('which-key').register({
-        ['<leader>nt'] = { '<cmd>FloatermNew<cr>', 'Open a new floating terminal' },
-      })
-
     end,
 }
