4b6f44e73210ee3280bb4896ec3fe0b7df68f102
simon-lentz <slentz445@gmail.com>
2024-06-27 09:24:51 -0400

    Adds autopairs and status bar plugins

---
+++ lua/plugins/autopairs.lua
@@ -0,0 +1,8 @@
+return {
+    -- https://github.com/windwp/nvim-autopairs
+    'windwp/nvim-autopairs',
+    event = "InsertEnter",
+    config = true
+    -- use opts = {} for passing setup options
+    -- this is equivalent to setup({}) function
+}
