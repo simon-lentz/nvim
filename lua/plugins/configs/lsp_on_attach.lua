9aa527f75c00452bce72c9bbd9b55bb950086cf5
simon-lentz <slentz445@gmail.com>
2024-06-27 15:47:03 -0400

    Restructures lsp config because the file was getting long.

---
+++ lua/plugins/configs/lsp_on_attach.lua
@@ -0,0 +1,14 @@
+local lsp_attach = function()
+    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
+    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
+    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0 })
+    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = 0 })
+
+    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
+    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
+
+    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { buffer = 0 })
+    end
+
+return lsp_attach
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -1,57 +1,19 @@
-local lsp_attach = function()
-    vim.keymap.set('n', 'K', vim.lsp.buf.hover, { buffer = 0 })
-    vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = 0 })
-    vim.keymap.set('n', 'gt', vim.lsp.buf.type_definition, { buffer = 0 })
-    vim.keymap.set('n', 'gr', vim.lsp.buf.references, { buffer = 0 })
-
-    vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, { buffer = 0 })
-    vim.keymap.set('n', '<leader>dn', vim.diagnostic.goto_next, { buffer = 0 })
-    vim.keymap.set('n', '<leader>dp', vim.diagnostic.goto_prev, { buffer = 0 })
-
-    vim.keymap.set('n', '<leader>dl', '<cmd>Telescope diagnostics<cr>', { buffer = 0 })
-    end
-
-local lua_opts = {
-	Lua = {
-		diagnostics = {
-			globals = { 'vim' }
-		}
-	}
-}
-
 return {
-    {
-	'hrsh7th/nvim-cmp',
- 	dependencies = {
-	  'hrsh7th/cmp-nvim-lsp',
-	  'hrsh7th/cmp-buffer',
-          'hrsh7th/cmp-path',
-	  {
-	    'L3MON4D3/LuaSnip',
-	    tag = 'v2.3.0',
-	  },
-	  'saadparwaiz1/cmp_luasnip',
-        },
-    },
-    {
 	'neovim/nvim-lspconfig',
+	depenencies = { 'hrsh7th/nvim-cmp' },
 	lazy = false,
 	config = function()
 	  local lspconfig = require('lspconfig')
-	  vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
-	  local cmp = require('cmp')
-	  cmp.setup({
-	    snippet = {
-	      expand = function(args)
-	        require('luasnip').lsp_expand(args.body)
-		end,
-	      }
-	    })
+
+          local lsp_attach = require('plugins.configs.lsp_on_attach')
+
 	  local caps = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
+
+	  local settings = require('plugins.configs.lsp_settings')
 
 	  lspconfig.lua_ls.setup({
 	    on_attach = lsp_attach,
-	    settings = lua_opts,
+	    settings = settings.lua,
 	    capabilities = caps,
 	    })
 
@@ -65,5 +27,4 @@ return {
 	    capabilities = caps,
 	    })
 	  end,
-    },
 }
