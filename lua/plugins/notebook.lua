405564dcd50ad940c89abedfc150555071b969a3
simon-lentz <slentz445@gmail.com>
2024-07-02 17:09:09 -0400

    Updates image.nvim configuration for notebook.lua.

--- lua/plugins/notebook.lua
+++ lua/plugins/notebook.lua
@@ -1,25 +1,13 @@
 return {
---[[  {
-    "bluz71/vim-moonfly-colors",
-    lazy = false,
-    priority = 1000,
-    config = function()
-      vim.cmd.syntax("enable")
-      vim.cmd.colorscheme("moonfly")
-
-      vim.api.nvim_set_hl(0, "MoltenOutputBorder", { link = "Normal" })
-      vim.api.nvim_set_hl(0, "MoltenOutputBorderFail", { link = "MoonflyCrimson" })
-      vim.api.nvim_set_hl(0, "MoltenOutputBorderSuccess", { link = "MoonflyBlue" })
-    end,
-  }, ]]--
   {
     "benlubas/molten-nvim",
-    -- dependencies = { "3rd/image.nvim" },
+    dependencies = {
+      require('plugins.configs.notebook_image'),
+    },
     build = ":UpdateRemotePlugins",
     init = function()
-      -- vim.g.molten_image_provider = "image.nvim"
+      vim.g.molten_image_provider = "image.nvim"
       vim.g.molten_use_border_highlights = true
-      -- add a few new things
 
       -- don't change the mappings (unless it's related to your bug)
       vim.keymap.set("n", "<localleader>mi", ":MoltenInit<CR>")
@@ -43,18 +31,4 @@ return {
       end, { desc = "Initialize Molten for python3", silent = true })
       end,
   },
---[[  {
-    "3rd/image.nvim",
-    opts = {
-      backend = "kitty",
-      integrations = {},
-      max_width = 100,
-      max_height = 12,
-      max_height_window_percentage = math.huge,
-      max_width_window_percentage = math.huge,
-      window_overlap_clear_enabled = true,
-      window_overlap_clear_ft_ignore = { "cmp_menu", "cmp_docs", "" },
-    },
-    version = "1.1.0", -- or comment out for latest
-		]]--
-} 
+}
